2ee0860951d1b54894b9d485e02ee5be641463c8
JotaveOliveira <jv.oa202@gmail.com>
2020-06-09 16:08:37 -0300

    Subindo teste

--- src/main/java/br/com/IHelp/Repository/PrestaServicoRepository.java
+++ src/main/java/br/com/IHelp/Repository/PrestaServicoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.IHelp.Entities.PrestaServico;
 
-public interface PrestaServicoRepository extends JpaRepository<PrestaServico, Long>{
+public interface PrestaServicoRepository extends JpaRepository<PrestaServico, Long> {
 
 }
